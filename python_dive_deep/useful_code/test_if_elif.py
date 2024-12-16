30219e0bab55fa9322feeeb2da0825f2b8f07a37
Rohit <rohitathithya@gmail.com>
2024-12-16 20:39:44 +0530

    test_if_elif.py>> ~~update the error in the code

--- python_dive_deep/useful_code/test_if_elif.py
+++ python_dive_deep/useful_code/test_if_elif.py
@@ -19,31 +19,4 @@ else:
         print("b is the greatest")
     else:
         if a<b and b<c:
-
-# if a>b:
-#     if a>c:
-#         print("a is greatest")
-#     else:
-#         print("c is the greatest")
-# else:
-#     if b>c:
-#         print("b is the greatest")
-#     else:
-#         print("c is the greatest")
-
-
-
-
-# 1
-# 1 2
-# 1 2 3
-# 1 2 3 4
-# 1 2 3 4 5
-
-
-
-#         1
-#       2 1
-#     3 2 1
-#   4 3 2 1
-# 5 4 3 2 1
+            print(' c is the greatest')
